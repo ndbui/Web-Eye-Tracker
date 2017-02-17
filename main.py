c5359315619c812eb56eee47d9cd9fa42bbab557
Nicholas Bui <nicholasbui14@gmail.com>
2017-02-16 20:23:05 -0500

    Created folder structure and gitignore

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import render_template, Flask
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+#setx FLASK_APP script.py
+#close out of bash
+#reopen bash and naviage to where the python script is located
+#flask run
+#I have no clue how to change the IP or port or anything else, we'll do tomorrow?
